efdb5bbdf7ea58315759b11639c138a282808bf3
Steams <radclifferobs@gmail.com>
2015-07-22 14:07:24 -0500

    added gradle script to export dependency jars to Eclipse class path

--- src/main/scala/com/steams/hello/MainActivity.scala
+++ src/main/scala/com/steams/hello/MainActivity.scala
@@ -3,14 +3,14 @@ package com.steams.hello
 import android.app.{ Activity,Fragment}
 import android.os.Bundle
 import android.view.{LayoutInflater,Menu,MenuItem,View,ViewGroup}
+import org.androidannotations.annotations.{ EActivity }
 
-
+//@EActivity( R.layout.fragment_main )
 class MainActivity extends Activity {
 
   override def onCreate( bundle:Bundle ){
     super.onCreate( bundle )
     setContentView( R.layout.fragment_main )
-
   }
 
 }
